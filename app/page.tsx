5558cbcbbda9008a6916deaec1436b2205bc5f58
goodness <goodyc474@gmail.com>
2023-04-21 14:03:40 +0100

    added bg-animation

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import SideBar from "./components/SideBar";
 import HomeSection from "./components/MainSection";
+import BackgroundAnimation from "./components/BgAnimation";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className="flex flex-col lg:flex-row  md:flex-row w-full m-auto justify-around my-3 p-3">
+      <BackgroundAnimation />
       <div className=" col-span-12  bg-gray-900  text-white  p-5  text-center  rounded-2xl ">
         <SideBar />
       </div>
