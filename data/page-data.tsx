cc0f79cee230e9de0bf0c774beba9090609dde9f
goodness <goodyc474@gmail.com>
2023-09-17 13:25:40 +0100

    ft: add more projects

--- data/page-data.tsx
+++ data/page-data.tsx
@@ -171,15 +171,15 @@ export const project: Iprojects[] = [
     github: "https://goodylove.github.io/real-estate-simple-home-page/",
     pageLink: "https://real-estate-simple-home-page.vercel.app/",
   },
-  {
-    name: "Cake website",
-    description:
-      "A cake website ,you use  the search secton to search any cake of your choice",
-    img: "/images/cake.JPG",
-    technology: ["html", "css", "javascript"],
-    github: "https://goodylove.github.io/simple-cake-website/",
-    pageLink: "https://simple-cake-website.vercel.app/",
-  },
+  // {
+  //   name: "Cake website",
+  //   description:
+  //     "A cake website ,you use  the search secton to search any cake of your choice",
+  //   img: "/images/cake.JPG",
+  //   technology: ["html", "css", "javascript"],
+  //   github: "https://goodylove.github.io/simple-cake-website/",
+  //   pageLink: "https://simple-cake-website.vercel.app/",
+  // },
   {
     name: "Quiz app",
     description: "A quiz app which has both English and Mathematics qestions.",
@@ -189,15 +189,15 @@ export const project: Iprojects[] = [
     pageLink: "https://quiz-app-seven-lac.vercel.app/",
   },
 
-  {
-    name: "Plant website",
-    description:
-      "A simple plant website  which i used to practice how to add item to cart and remove items from cart using react-app",
-    img: "/images/e-commerce.JPG",
-    technology: ["react", "tailwind css"],
+  // {
+  //   name: "Plant website",
+  //   description:
+  //     "A simple plant website  which i used to practice how to add item to cart and remove items from cart using react-app",
+  //   img: "/images/e-commerce.JPG",
+  //   technology: ["react", "tailwind css"],
 
-    pageLink: "https://igbobo.vercel.app/",
-  },
+  //   pageLink: "https://igbobo.vercel.app/",
+  // },
   // {
   //   name: "simple website",
   //   img: "/images/cart.JPG",
