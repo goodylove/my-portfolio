bc399c547955a5ee54a2e84d1876eb780852daa1
goodness <goodyc474@gmail.com>
2023-09-26 16:53:16 +0100

    bg:  fix overflow

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <Provider>
-      <main className="flex flex-col lg:flex-row  md:flex-row w-full m-auto justify-around my-3 p-3">
+      <main className="flex flex-col lg:flex-row  md:flex-row w-full m-auto justify-around my-3 p-3 overflow-x-hidden">
         <BackgroundAnimation />
         <div className=" col-span-12  bg-gray-900  text-white  p-5  text-center  rounded-2xl dark:bg-[#02855a]  dark:shadow-lg ">
           <SideBar />
